9461cc67657c2eac85dea29f703e0a6a17e841b1
hpiso <hugopiso@gmail.com>
2016-03-08 20:35:39 +0100

    fonction suppr

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,8 +4,11 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Item;
 use AppBundle\Form\ItemType;
+use Doctrine\ORM\EntityNotFoundException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
@@ -27,10 +30,48 @@ class DefaultController extends Controller
             $em->flush();
         }
 
-
         return $this->render('AppBundle:App/index.html.twig', [
             'items' => $items,
             'form'  => $form->createView()
         ]);
+    }
+
+    /**
+     * Deletes a Actualite entity.
+     *
+     * @Route("/conso-delete/{id}", name="conso_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em   = $this->getDoctrine()->getManager();
+            $item = $em->getRepository('AppBundle:Item')->find($id);
+
+            if (!$item) {
+                throw new EntityNotFoundException('Entity item not found');
+            }
+
+            $em->remove($item);
+            $em->flush();
+
+            $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('AppBundle:App/delete-form.html.twig', [
+            'form'  => $form->createView()
+        ]);
+    }
+
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder()
+            ->setMethod('DELETE')
+            ->add('submit', SubmitType::class, ['label' => 'Supprimer quand même', 'attr' => ['class' => 'button']])
+            ->getForm()
+            ;
     }
 }
