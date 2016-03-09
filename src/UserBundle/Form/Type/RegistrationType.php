1a6e0cf81c139d1b808f8e3969b5a1a42fe93c26
hpiso <hugopiso@gmail.com>
2016-03-09 20:34:58 +0100

    ajout du layout login

--- src/UserBundle/Form/Type/RegistrationType.php
+++ src/UserBundle/Form/Type/RegistrationType.php
@@ -4,6 +4,8 @@ namespace UserBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Tests\Extension\Core\Type\PasswordTypeTest;
+use Symfony\Component\Form\Tests\Extension\Core\Type\RepeatedTypeTest;
 
 class RegistrationType extends AbstractType
 {
